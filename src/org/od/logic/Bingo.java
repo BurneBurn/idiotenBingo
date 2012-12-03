1ec26fc4cca59ec8d5fa7924f692d831421bf44e
SchnapsPeach <olga@dedi.de>
2012-12-03 15:05:39 +0100

    Erste Klasse

---
+++ src/org/od/logic/Bingo.java
@@ -0,0 +1,12 @@
+package org.od.logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bingo {
+	List<String> field = new ArrayList<String>();
+	
+	public Bingo() {
+		field.add("Dummlabern");
+	}
+}
